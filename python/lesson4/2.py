7f16235f9473d31f088f2208c26b6197e1b2e482
DeNShch00 <denis.shchelakov@mail.ru>
2020-03-05 11:40:02 +0300

    Bugfix task 2:  out_list gen doesn't  process first element of in_list

--- python/lesson4/2.py
+++ python/lesson4/2.py
@@ -6,5 +6,5 @@
 """
 
 in_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
-out_list = [in_list[i] for i in range(2, len(in_list)) if in_list[i] > in_list[i-1]]
+out_list = [in_list[i] for i in range(1, len(in_list)) if in_list[i] > in_list[i-1]]
 print(out_list)
