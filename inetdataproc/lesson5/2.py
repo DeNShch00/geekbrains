96720e2f870afb406b420c0dc9108706b3c0d35a
DeNShch00 <denis.shchelakov@mail.ru>
2020-09-16 01:41:16 +0300

    use hits buttons

--- inetdataproc/lesson5/2.py
+++ inetdataproc/lesson5/2.py
@@ -5,18 +5,18 @@
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException, ElementClickInterceptedException
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 from pymongo import MongoClient
+import time
 
 
-# TODO: untested version !!!!
-
-
-def wait_element_by_class_name(drv, name):
-    return WebDriverWait(drv, 20).until(
-        expected_conditions.presence_of_element_located((By.CLASS_NAME, name))
+def wait_clickable_element_by_class_name(drv, name, timeout_sec=20):
+    return WebDriverWait(drv, timeout_sec).until(
+        expected_conditions.element_to_be_clickable((By.CLASS_NAME, name))
     )
 
 
@@ -24,26 +24,47 @@ chrome_options = Options()
 chrome_options.add_argument('start-maximized')
 
 driver = webdriver.Chrome('./chromedriver.exe', options=chrome_options)
-url = 'https://www.mvideo.ru'
-driver.get(url)
+driver.get('https://www.mvideo.ru')
+
+hits_block = None
+timeout_sec = 20.0
+while timeout_sec > 0:
+    products_blocks = driver.find_elements_by_class_name('sel-hits-block')
+    if len(products_blocks) < 2:
+        time.sleep(0.1)
+        timeout_sec -= 0.1
+    else:
+        hits_block = products_blocks[1]
+        break
+
+actions = ActionChains(driver)
+actions.move_to_element(hits_block)
+actions.perform()
 
 while True:
     try:
-        hits_btn = wait_element_by_class_name(driver, 'sel-hits-button-next')
-        hits_btn.click()
-    except:
+        hits_btn_next = wait_clickable_element_by_class_name(hits_block, 'sel-hits-button-next')
+        hits_btn_next.click()
+        attrs = hits_btn_next.get_attribute('class')
+        if 'disabled' in attrs:
+            break
+
+    except ElementClickInterceptedException:
+        continue
+    except TimeoutException:
         break
 
 mongo = MongoClient('localhost', 27017)
-db = mongo['mvideo_hits']
+db = mongo['mvideo_hits_db']
 mvideo_hits_db = db.mvideo_hits
 
-for item in driver.find_elements_by_xpath("//div[contains(@class, 'sel-hits-block')]//li"):
+for item in hits_block.find_elements_by_tag_name('li'):
     name = item.find_element_by_class_name('sel-product-tile-title').text
-    link = url + item.find_element_by_class_name('sel-product-tile-title').get_attribute('href')
+    link = item.find_element_by_class_name('sel-product-tile-title').get_attribute('href')
     price = item.find_element_by_class_name('c-pdp-price__current').text
-    price = int(price.replace(' ', ''))
-    mvideo_hits_db.insert_one({'name': name, 'link': link, 'price': price})
+    # price = int(price.replace(' ', ''))
+    # mvideo_hits_db.insert_one({'name': name, 'link': link, 'price': price})
+    print({'name': name, 'link': link, 'price': price})
 
 for hit in mvideo_hits_db.find({}):
     print(hit)
